8f796ec3980d53de0ee4d7d18ad3c288e782365d
ru-the-dev <dev.ru.vero@gmail.com>
2026-01-04 12:29:23 +0100

    refactor: improve WardrobeTransmogFrame camera zoom and button positioning
    - Hook OrbitCameraMixin.SetMinZoomDistance to prevent Blizzard from resetting
      zoom distance back to 2, allowing custom zoom limits on the transmog model
    - Call ApplyFromModelSceneCameraInfo hook to set max zoom to 7.5 and default
      zoom to 4 after camera initialization completes
    - Rename UpdateTransmogSlotPositions to PositionTransmogButtons for clarity
    - Consolidate button positioning logic into single reusable function
    - Update OnSizeChanged event to recalculate button positions dynamically

--- Frames/WardrobeTransmogFrame.lua
+++ Frames/WardrobeTransmogFrame.lua
@@ -16,96 +16,86 @@ end
 WardrobeTransmogFrame = LibRu.EventFrame.New(WardrobeTransmogFrame);
 
 -- Button arrays for positioning
-local buttonsLeft = {
-    WardrobeTransmogFrame.HeadButton,
-    WardrobeTransmogFrame.ShoulderButton,
-    WardrobeTransmogFrame.BackButton,
-    WardrobeTransmogFrame.ChestButton,
-    WardrobeTransmogFrame.ShirtButton,
-    WardrobeTransmogFrame.TabardButton,
-    WardrobeTransmogFrame.WristButton
-};
-
-local buttonsRight = {
-    WardrobeTransmogFrame.HandsButton,
-    WardrobeTransmogFrame.WaistButton,
-    WardrobeTransmogFrame.LegsButton,
-    WardrobeTransmogFrame.FeetButton
+local slotButtons = {
+    Left = {
+        WardrobeTransmogFrame.HeadButton,
+        WardrobeTransmogFrame.ShoulderButton,
+        WardrobeTransmogFrame.BackButton,
+        WardrobeTransmogFrame.ChestButton,
+        WardrobeTransmogFrame.ShirtButton,
+        WardrobeTransmogFrame.TabardButton,
+        WardrobeTransmogFrame.WristButton
+    },
+    Right = {
+        WardrobeTransmogFrame.HandsButton,
+        WardrobeTransmogFrame.WaistButton,
+        WardrobeTransmogFrame.LegsButton,
+        WardrobeTransmogFrame.FeetButton
+    }
 }
 
--- Position left side buttons
-local function PositionLeftButtons(yOffset, ySpacing)
-    -- first left button
-    buttonsLeft[1]:SetPoint("TOPLEFT", WardrobeTransmogFrame, "TOPLEFT", 28, -yOffset);
-    
-    -- other left buttons
-    for i = 2, (#buttonsLeft) do
-        buttonsLeft[i]:SetPoint("TOP", buttonsLeft[i - 1], "BOTTOM", 0, -ySpacing);
-    end
-end
-
--- Position right side buttons
-local function PositionRightButtons(yOffset, ySpacing)
-    -- first right button
-    buttonsRight[1]:SetPoint("TOPRIGHT", WardrobeTransmogFrame, "TOPRIGHT", -28, -yOffset);
-
-    -- other right buttons
-    for i = 2, (#buttonsRight) do
-        buttonsRight[i]:SetPoint("TOP", buttonsRight[i - 1], "BOTTOM", 0, -ySpacing);
-    end
-end
-
--- Position secondary shoulder button
-local function PositionSeperateShoulderButton()
-    WardrobeTransmogFrame.SecondaryShoulderButton:ClearAllPoints();
-    WardrobeTransmogFrame.SecondaryShoulderButton:SetPoint("LEFT", WardrobeTransmogFrame.ShoulderButton, "RIGHT", 15, 0);
-end
-
--- Position weapon buttons
-local function PositionWeaponButtons()
-    -- main hand + mainEnchant
-    WardrobeTransmogFrame.MainHandButton:ClearAllPoints();
-    WardrobeTransmogFrame.MainHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame, "BOTTOM", -28, 24);
-    
-    WardrobeTransmogFrame.MainHandEnchantButton:ClearAllPoints();
-    WardrobeTransmogFrame.MainHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.MainHandButton, "TOP", 0, -5);
-
-    -- off-hand + offEnchant
-    WardrobeTransmogFrame.SecondaryHandButton:ClearAllPoints();
-    WardrobeTransmogFrame.SecondaryHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame, "BOTTOM", 28, 24);
-    
-    WardrobeTransmogFrame.SecondaryHandEnchantButton:ClearAllPoints();
-    WardrobeTransmogFrame.SecondaryHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.SecondaryHandButton, "TOP", 0, -5);
-end
-
--- Handle overall sizing and button positioning
-local function HandleButtonSizing(width, height)
-    local yOffsetScalar = 0.1;
-    local yOffset = height * yOffsetScalar;
-    local ySpacing = (height * (1 - yOffsetScalar) - (#buttonsLeft * 60)) / #buttonsLeft
-   
-    PositionLeftButtons(yOffset, ySpacing);
-    PositionSeperateShoulderButton();
-    PositionRightButtons(yOffset, ySpacing);
-    PositionWeaponButtons();
-    
-    -- double shoulder xmog toggle
-    WardrobeTransmogFrame.ToggleSecondaryAppearanceCheckbox:ClearAllPoints();
-    WardrobeTransmogFrame.ToggleSecondaryAppearanceCheckbox:SetPoint("BOTTOMLEFT", WardrobeCollectionFrame, "BOTTOMLEFT", 20, 20);
-end
-
--- Initialize button positioning
-WardrobeTransmogFrame.HeadButton:ClearAllPoints();
-
--- set new anchor points
+-- set new anchor points for the WardrobeTransmogFrame
 WardrobeTransmogFrame:ClearAllPoints();
 WardrobeTransmogFrame:SetPoint("TOPLEFT", WardrobeFrame, "TOPLEFT", 4, -86);
 WardrobeTransmogFrame:SetPoint("BOTTOMLEFT", WardrobeFrame, "BOTTOMLEFT", 4, 25);
 
+-- set new anchor points for the ModelScene
+WardrobeTransmogFrame.ModelScene:SetPoint("TOPLEFT", WardrobeTransmogFrame, "TOPLEFT");
+WardrobeTransmogFrame.ModelScene:SetPoint("BOTTOMRIGHT", WardrobeTransmogFrame, "BOTTOMRIGHT");
+
+-- increase max zoom out distance on the modelscene
+hooksecurefunc(WardrobeTransmogFrame.ModelScene, "SetActiveCamera", function(self, camera)
+    -- Hook this specific camera instance to catch SetMinZoomDistance calls
+    hooksecurefunc(camera, "ApplyFromModelSceneCameraInfo", function(self, modelSceneCameraInfo, transitionType, modificationType)
+        _G.BetterTransmog.DebugLog("Setting min zoom distance for WardrobeTransmogFrame ModelScene camera");
+        self:SetMaxZoomDistance(7.5);
+        self:SetZoomDistance(4);
+    end)
+end)
+
+
+
+-- adjust Inset background to fit new frame size
 local InsetBG = WardrobeTransmogFrame.Inset.BG;
 InsetBG:ClearAllPoints();
 InsetBG:SetPoint("TOPLEFT", WardrobeTransmogFrame.Inset, "TOPLEFT", 1, -1);
 InsetBG:SetPoint("BOTTOMRIGHT", WardrobeTransmogFrame.Inset, "BOTTOMRIGHT", -1, 1);
+
+
+-- function to update transmog slot button positions based on frame height
+local function UpdateTransmogSlotPositions(height)
+    local yOffsetScalar = 0.1;
+    local yOffset = height * yOffsetScalar;
+    local ySpacing = (height * (1 - yOffsetScalar) - (#slotButtons.Left * 60)) / #slotButtons.Left
+
+    -- Position left side buttons
+    slotButtons.Left[1]:SetPoint("TOPLEFT", WardrobeTransmogFrame, "TOPLEFT", 28, -yOffset);
+    for i = 2, (#slotButtons.Left) do
+        slotButtons.Left[i]:SetPoint("TOP", slotButtons.Left[i - 1], "BOTTOM", 0, -ySpacing);
+    end
+    
+    -- Position secondary shoulder button
+    WardrobeTransmogFrame.SecondaryShoulderButton:ClearAllPoints();
+    WardrobeTransmogFrame.SecondaryShoulderButton:SetPoint("LEFT", WardrobeTransmogFrame.ShoulderButton, "RIGHT", 15, 0);
+    
+    -- Position right side buttons
+    slotButtons.Right[1]:SetPoint("TOPRIGHT", WardrobeTransmogFrame, "TOPRIGHT", -28, -yOffset);
+    for i = 2, (#slotButtons.Right) do
+        slotButtons.Right[i]:SetPoint("TOP", slotButtons.Right[i - 1], "BOTTOM", 0, -ySpacing);
+    end
+    
+    -- Position weapon buttons
+    WardrobeTransmogFrame.MainHandButton:ClearAllPoints();
+    WardrobeTransmogFrame.MainHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame, "BOTTOM", -28, 24);
+    WardrobeTransmogFrame.MainHandEnchantButton:ClearAllPoints();
+    WardrobeTransmogFrame.MainHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.MainHandButton, "TOP", 0, -5);
+    
+    WardrobeTransmogFrame.SecondaryHandButton:ClearAllPoints();
+    WardrobeTransmogFrame.SecondaryHandButton:SetPoint("BOTTOM", WardrobeTransmogFrame, "BOTTOM", 28, 24);
+    WardrobeTransmogFrame.SecondaryHandEnchantButton:ClearAllPoints();
+    WardrobeTransmogFrame.SecondaryHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.SecondaryHandButton, "TOP", 0, -5);
+end
+
 
 WardrobeFrame:AddScript("OnSizeChanged", function(handle, width, height)
     local TransmogFrameConfig = _G.BetterTransmog.DB.Account.TransmogFrame;
@@ -113,13 +103,7 @@ WardrobeFrame:AddScript("OnSizeChanged", function(handle, width, height)
     WardrobeTransmogFrame:SetWidth(newWidth);
     
     -- Position buttons based on new size
-    HandleButtonSizing(newWidth, height);
+    UpdateTransmogSlotPositions(height);
 end)
 
-_G.BetterTransmog.EventFrame:AddScript("OnAccountDBInitialized", function(handle)
-    -- Initial button positioning
-    local width = WardrobeTransmogFrame:GetWidth();
-    local height = WardrobeFrame:GetHeight();
-    HandleButtonSizing(width, height);
-end)
-
+UpdateTransmogSlotPositions(WardrobeTransmogFrame:GetWidth());
