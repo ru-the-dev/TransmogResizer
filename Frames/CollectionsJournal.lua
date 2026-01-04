a2a6056fb6c3b0b3962756d6d832a43f888762cb
ru-the-dev <dev.ru.vero@gmail.com>
2026-01-04 13:50:47 +0100

    refactor: reorganize initialization checks and improve layout handling in CollectionsJournal and WardrobeCollectionFrame

--- Frames/CollectionsJournal.lua
+++ Frames/CollectionsJournal.lua
@@ -1,28 +1,15 @@
--- if not _G.BetterTransmog then
---     error("BetterTransmog must be initialized before TransmogModelScene.lua. Please ensure Initialize.lua is loaded first.")
--- end
+if not _G.BetterTransmog then
+    error("BetterTransmog must be initialized before TransmogModelScene.lua. Please ensure Initialize.lua is loaded first.")
+end
 
--- --- @type LibRu
--- local LibRu = _G.LibRu
+--- @type LibRu
+local LibRu = _G.LibRu
 
--- if not LibRu then
---     error("LibRu is required to initialize BetterTransmog. Please ensure LibRu is loaded before BetterTransmog.lua")
--- end
+if not LibRu then
+    error("LibRu is required to initialize BetterTransmog. Please ensure LibRu is loaded before BetterTransmog.lua")
+end
 
 
--- -- convert wardrobe frame to a eventframe
--- CollectionsJournal = LibRu.EventFrame.New(CollectionsJournal);
-
--- -- Configure the UI panel layout to work with our custom frame
--- UIPanelWindows["CollectionsJournal"] = {
---     area = "center",         -- Center so it doesn't push other panels
---     pushable = 0,            -- Don't let other panels push us around
---     whileDead = 1,           -- Can be shown while dead
---     width = CollectionsJournal:GetWidth(), -- Use actual width
---     xOffset = 0,
---     yOffset = 0,
---     allowOtherPanels = 1     -- Allow other panels to be open
--- }
 
 -- -- set the CollectionsJournal to be movable
 -- CollectionsJournal:SetMovable(true);
--- Frames/WardrobeCollectionFrame.lua
+++ Frames/WardrobeCollectionFrame.lua
@@ -13,42 +13,41 @@ end
 
 -- override SetContainer to adjust layout based on parent frame
 
+
 function WardrobeCollectionFrame:SetContainer(parent)
-
-    if self:GetParent() == parent then 
-        self:Show()
-        return
-    end
-
     _G.BetterTransmog.DebugLog("WardrobeCollectionFrame:SetContainer called with parent: " .. tostring(parent));
 
     self:SetParent(parent);
-	self:ClearAllPoints();
-	if parent == CollectionsJournal then
-		self:SetPoint("TOPLEFT", CollectionsJournal);
-		self:SetPoint("BOTTOMRIGHT", CollectionsJournal);
-		self.ItemsCollectionFrame.ModelR1C1:SetPoint("TOP", -238, -94);
-		self.ItemsCollectionFrame.PagingFrame:SetPoint("BOTTOM", 22, 29);
-		self.ItemsCollectionFrame.SlotsFrame:Show();
-		self.ItemsCollectionFrame.BGCornerTopLeft:Hide();
-		self.ItemsCollectionFrame.BGCornerTopRight:Hide();
-		self.ItemsCollectionFrame.WeaponDropdown:SetPoint("TOPRIGHT", -25, -58);
-		self.ClassDropdown:Show();
-		self.ItemsCollectionFrame.NoValidItemsLabel:Hide();
-		self.ItemsTab:SetPoint("TOPLEFT", 58, -28);
-		self:SetTab(self.selectedCollectionTab);
-	elseif parent == WardrobeFrame then
-		self:SetPoint("TOPLEFT", WardrobeTransmogFrame, "TOPRIGHT", -5, 60);
+    self:ClearAllPoints();
+    
+    if parent == CollectionsJournal then
+        -- Clear any custom anchoring from WardrobeFrame to prevent conflicts
+        WardrobeFrame:ClearAllPoints();
+        
+        self:SetPoint("TOPLEFT", CollectionsJournal);
+        self:SetPoint("BOTTOMRIGHT", CollectionsJournal);
+        self.ItemsCollectionFrame.ModelR1C1:SetPoint("TOP", -238, -94);
+        self.ItemsCollectionFrame.PagingFrame:SetPoint("BOTTOM", 22, 29);
+        self.ItemsCollectionFrame.SlotsFrame:Show();
+        self.ItemsCollectionFrame.BGCornerTopLeft:Hide();
+        self.ItemsCollectionFrame.BGCornerTopRight:Hide();
+        self.ItemsCollectionFrame.WeaponDropdown:SetPoint("TOPRIGHT", -25, -58);
+        self.ClassDropdown:Show();
+        self.ItemsCollectionFrame.NoValidItemsLabel:Hide();
+        self.ItemsTab:SetPoint("TOPLEFT", 58, -28);
+        self:SetTab(self.selectedCollectionTab);
+    elseif parent == WardrobeFrame then
+        self:SetPoint("TOPLEFT", WardrobeTransmogFrame, "TOPRIGHT", -5, 60);
         self:SetPoint("BOTTOMRIGHT", WardrobeFrame, "BOTTOMRIGHT", 0, 0);
-		self.ItemsCollectionFrame.ModelR1C1:SetPoint("TOP", -235, -71);
-		self.ItemsCollectionFrame.PagingFrame:SetPoint("BOTTOM", 22, 38);
-		self.ItemsCollectionFrame.SlotsFrame:Hide();
-		self.ItemsCollectionFrame.BGCornerTopLeft:Show();
-		self.ItemsCollectionFrame.BGCornerTopRight:Show();
-		self.ItemsCollectionFrame.WeaponDropdown:SetPoint("TOPRIGHT", -48, -26);
-		self.ClassDropdown:Hide();
-		self.ItemsTab:SetPoint("TOPLEFT", 8, -28);
-		self:SetTab(self.selectedTransmogTab);
-	end
-	self:Show();
+        self.ItemsCollectionFrame.ModelR1C1:SetPoint("TOP", -235, -71);
+        self.ItemsCollectionFrame.PagingFrame:SetPoint("BOTTOM", 22, 38);
+        self.ItemsCollectionFrame.SlotsFrame:Hide();
+        self.ItemsCollectionFrame.BGCornerTopLeft:Show();
+        self.ItemsCollectionFrame.BGCornerTopRight:Show();
+        self.ItemsCollectionFrame.WeaponDropdown:SetPoint("TOPRIGHT", -48, -26);
+        self.ClassDropdown:Hide();
+        self.ItemsTab:SetPoint("TOPLEFT", 8, -28);
+        self:SetTab(self.selectedTransmogTab);
+    end
+    self:Show();
 end
